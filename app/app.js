cee515224476cc4a8680fb145a472917945b339b
dbetke <danielle.betke@gmail.com>
2013-01-18 08:24:27 -0500

    changes to use jade view engine instead of ejs view engine, adds jade version of layout, index, about, contact, contribute, and search pages

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@
  */
 
 var express = require('express')
-  , ejs = require('ejs')
+  //, ejs = require('ejs')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
@@ -15,7 +15,8 @@ var app = express();
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
+ // app.set('view engine', 'ejs');
+  app.set('view engine', 'jade');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
@@ -32,6 +33,9 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 app.get('/search', routes.search);
+app.get('/contribute', routes.contribute);
+app.get('/about', routes.about);
+app.get('/contact', routes.contact);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,25 @@
 /* index page */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Know Your Food' })
+  res.render('index', { title: 'Know Your Food', nbsp: ' ' })
+};
+
+/* search page */
+exports.search = function(req, res){
+  res.render('search', { title: 'Search Food', nbsp: ' ' })
+};
+
+/* contribute page */
+exports.contribute = function(req, res){
+  res.render('contribute', { title: 'Contribute', nbsp: ' ' })
+};
+
+/* about page */
+exports.about = function(req, res){
+  res.render('about', { title: 'About Us', nbsp: ' ' })
+};
+
+/* contact page */
+exports.contact = function(req, res){
+  res.render('contact', { title: 'Contact Us', nbsp: ' ' })
 };
