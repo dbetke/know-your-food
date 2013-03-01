7cda42fe7b35c16453a2fec5e7cf457d509c965d
dbetke <danielle.betke@gmail.com>
2013-03-01 07:56:49 -0500

    updates fixtures file to include a few products

--- fixtures.js
+++ fixtures.js
@@ -1,11 +1,18 @@
-var ProductController = require('./controllers/products');
-var mongoose = require('mongoose');
-
-var mongoServer = 'mongodb://localhost/Products';
+var ProductController = require('./controllers/products'), 
+    mongoose = require('mongoose'),
+    mongoServer = 'mongodb://localhost/Products',
+    pc = new ProductController;
 
 mongoose.connect(mongoServer);
 
-//THIS IS TESTING THE ABILITY TO SAVE PRODUCTS TO THE DATABASE FROM A SCRIPT FILE RATHER THAN FROM THE WEB PAGE.  IN PROGRESS. ADDED THE TEST PRODUCT SUCCESSFULLY.
-ProductController.saveProducts({ brandname: 'testbrand', productname: 'testproduct', ingredients: 'test ingredient 1, test ingredient 2' }, function(res){
-    console.log(res);
-});
+pc.saveProducts({ brandname: 'Progresso',
+		  productname: 'Vegetable Classics Creamy Mushroom',
+		  ingredients: 'Water, Portabella Mushrooms, Soybean Oil, Modified Food Starch, Cream, Soy Protein Concentrate, Mushroom Extract, Salt, Butter, Sugar, Sodium Phosphate, Modified Whey Protein Concentrate, Dried Parsley, Onion Powder, Garlic Powder, Yeast Extract, Spice'});
+
+pc.saveProducts({ brandname: "Amy's",
+		  productname: 'Cream of Mushroom Soup',
+		  ingredients: 'filtered water, organic mushrooms, organic onions, organic wheat flour, organic high oleic safflower and/or sunflower oil, organic leeks, organic grade AA butter (cream, salt, annatto [color]), organic cream, spices (100% pure herbs & spices (no hidden ingredients)), sea salt, organic garlic, organic black pepper'});
+
+pc.saveProducts({ brandname: "Campbell's",
+		  productname: 'Cream of Mushroom Soup',
+		  ingredients: 'water, mushrooms, Modified Food Starch, wheat flour, salt, cream, dried whey, Monosodium Glutamate, soy protein concentrate, yeast extract, spice extract, dehydrated garlic, vegetable oil'});
