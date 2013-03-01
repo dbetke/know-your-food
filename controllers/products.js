ea57b6c98ad6f2f31f925ae66de2f16f2d34c6de
dbetke <danielle.betke@gmail.com>
2013-03-01 11:06:12 -0500

    combines product controller save methods

--- controllers/products.js
+++ controllers/products.js
@@ -2,7 +2,7 @@ var ProductController = function(){
     var Product = require('../models/product');
 
     function strip(item){
-	return item.replace(/['";:.\/?\\-]/g, '');
+	return item.replace(/['";:.\/?\\-\s]/g, '');
     };
 
     //view everything stored in the database
@@ -17,60 +17,6 @@ var ProductController = function(){
 	});
     };
 
-    this.saveProduct = function(req, res)  {
-	var brandname = req.body.brandname.toUpperCase();
-	var brandname_stripped = strip(brandname);
-	var productname = req.body.productname.toUpperCase();
-	var productname_stripped = strip(productname);
-	var ingredients = req.body.ingredients.toLowerCase();
-	var ingredients_stripped = strip(ingredients);
-	
-	//parse ingredients by commas and store in array
-	var ingredients_array = ingredients.split(',');
-	var ingredients_stripped_array = ingredients_stripped.split(',');
-	
-	
-	//require all fields
-	if ((brandname != "") && (productname != "") && (ingredients != "")){
-	    
-	    //Test if product already exists
-	    Product.find({brandname_stripped: brandname_stripped, productname_stripped: productname_stripped}, function(err, products){
-		if(err){
-		    res.send('bad news: ' + err.message);
-		}
-		else if (products != ""){
-		    res.send("Oops.  This product already exists");
-		}
-		else{
-		    
-		    //create the new product
-		    var newProduct = new Product({
-			'brandname' : brandname,
-			'brandname_stripped' : brandname_stripped,
-			'productname' : productname,
-			'productname_stripped' : productname_stripped,
-			'ingredients' : ingredients_array,
-			'ingredients_stripped' : ingredients_stripped_array
-		    });
-		    
-		    //save the new product to the database
-		    newProduct.save(function (err, newProduct) {
-			if (err){
-			    res.send(err);
-			}
-			else{
-			    //send confirmation that product was saved
-			    res.send("The following was written to the database: \n" + newProduct);
-			}
-		    });
-		}
-	    });
-	}
-	else{
-	    res.send("All fields are required");
-	}
-    };
-    
     //finds products based on search criteria
     this.findProduct = function(req, res){
 	var brandname = strip(req.body.brandname.toUpperCase());
@@ -178,33 +124,59 @@ var ProductController = function(){
 	
     };
     
-    //TODO: THIS METHOD IS TO SAVE PRODUCTS FROM THE SCRIPT FILE RATHER THAN THE WEB PAGE. NOT SURE IF THIS IS THE BEST WAY TO HANDLE 
-    this.saveProducts = function(req, res)  {
-	var brandname = req.brandname.toUpperCase();
-	var brandname_stripped = strip(req.brandname.toUpperCase());
-	var productname = req.productname.toUpperCase();
-	var productname_stripped = strip(req.productname.toUpperCase()); 
-	var ingredients = req.ingredients.toLowerCase();
-	var ingredients_stripped = strip(req.ingredients.toLowerCase());
+    this.saveProduct = function(req, res)  {
+	var brandname,
+	    brandname_stripped,
+	    productname,
+	    productname_stripped,
+	    ingredients,
+	    ingredients_stripped,
+	    ingredients_array,
+	    ingredients_stripped_array,
+            send; 
+
+	//check if req is from file or web    
+	if (req.brandname){
+	    brandname = req.brandname.toUpperCase();
+	    productname = req.productname.toUpperCase();
+	    ingredients = req.ingredients.toLowerCase();
+	    send = function (msg){
+		console.log(msg);
+	    }
+	}
+	else{
+	    brandname = req.body.brandname.toUpperCase();
+	    productname = req.body.productname.toUpperCase();
+	    ingredients = req.body.ingredients.toLowerCase();
+	    send = function (msg) {
+		res.send(msg);
+	    };
+	}	
+
+	brandname_stripped = strip(brandname);
+	productname_stripped = strip(productname); 
+	ingredients_stripped = strip(ingredients);
 	
 	//parse ingredients by commas and store in array
-	var ingredients_array = ingredients.split(',');
-	var ingredients_stripped_array = ingredients_stripped.split(',');
+	ingredients_array = ingredients.split(',');
+	ingredients_stripped_array = ingredients_stripped.split(',');
 	
 	
 	//require all fields
 	if ((brandname != "") && (productname != "") && (ingredients != "")){
 	    
 	    //Test if product already exists
-	    Product.find({brandname_stripped: brandname_stripped, productname_stripped: productname_stripped}, function(err, products){
+	    Product.count({ brandname_stripped: brandname_stripped, productname_stripped: productname_stripped }, function (err, count) {
 		if(err){
-		    console.log('bad news: ' + err.message);
+		    send(err);
 		}
-		else if (products != ""){
-		    console.log("Oops.  This product already exists");
+
+		else if (count >= 1){
+		    send("The following product already exists in the database and was not saved: \n  ~  " + brandname + " " + productname);
 		}
+
 		else{
-		    
+
 		    //create the new product
 		    var newProduct = new Product({
 			'brandname' : brandname,
@@ -218,20 +190,20 @@ var ProductController = function(){
 		    //save the new product to the database
 		    newProduct.save(function (err, newProduct) {
 			if (err){
-			    console.log(err);
+			    send(err);
 			}
 			else{
 			    //send confirmation that product was saved
-			    console.log("The following was written to the database: \n" + newProduct);
+			    send("The following was written to the database: \n" + newProduct);
 			}
 		    });
+
 		}
 	    });
 	}
 	else{
-	    console.log("All fields are required");
+	    send("All fields are required");
 	}
-	
     };
 };
 
--- fixtures.js
+++ fixtures.js
@@ -5,14 +5,15 @@ var ProductController = require('./controllers/products'),
 
 mongoose.connect(mongoServer);
 
-pc.saveProducts({ brandname: 'Progresso',
+pc.saveProduct({ brandname: 'Progresso',
 		  productname: 'Vegetable Classics Creamy Mushroom',
 		  ingredients: 'Water, Portabella Mushrooms, Soybean Oil, Modified Food Starch, Cream, Soy Protein Concentrate, Mushroom Extract, Salt, Butter, Sugar, Sodium Phosphate, Modified Whey Protein Concentrate, Dried Parsley, Onion Powder, Garlic Powder, Yeast Extract, Spice'});
 
-pc.saveProducts({ brandname: "Amy's",
+pc.saveProduct({ brandname: "Amy's",
 		  productname: 'Cream of Mushroom Soup',
 		  ingredients: 'filtered water, organic mushrooms, organic onions, organic wheat flour, organic high oleic safflower and/or sunflower oil, organic leeks, organic grade AA butter (cream, salt, annatto [color]), organic cream, spices (100% pure herbs & spices (no hidden ingredients)), sea salt, organic garlic, organic black pepper'});
 
-pc.saveProducts({ brandname: "Campbell's",
+pc.saveProduct({ brandname: "Campbell's",
 		  productname: 'Cream of Mushroom Soup',
 		  ingredients: 'water, mushrooms, Modified Food Starch, wheat flour, salt, cream, dried whey, Monosodium Glutamate, soy protein concentrate, yeast extract, spice extract, dehydrated garlic, vegetable oil'});
+
